f064cdde9cb6ec216987f7c33fb540d20dcfe786
manishbaghel <manishbaghel@gmail.com>
2017-04-10 23:53:07 -0500

    Ubuntu -Networking lession

---
+++ javaPractice/src/main/java/learn/url/URLReader.java
@@ -0,0 +1,19 @@
+package learn.url;
+
+import java.net.*;
+import java.io.*;
+
+public class URLReader {
+    public static void main(String[] args) throws Exception {
+
+        URL oracle = new URL("http://www.google.com/");
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
+
